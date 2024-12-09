f509881c207abaa99c6383e5e22c9fcec1f90a07
Mark Parker <msparker@sky.com>
2024-12-09 13:00:14 +0000

    fix: remove eth-utils requirement

--- dimo/eth_signer.py
+++ dimo/eth_signer.py
@@ -1,6 +1,8 @@
+from typing import NewType
 from eth_account import Account
 from eth_account.messages import encode_defunct
-from eth_utils import to_bytes, remove_0x_prefix, add_0x_prefix
+
+HexStr = NewType("HexStr", str)
 
 
 class EthSigner:
@@ -12,3 +14,23 @@ class EthSigner:
         signed_message = account.sign_message(message_hash)
 
         return add_0x_prefix(signed_message.signature.hex())
+
+
+def is_0x_prefixed(value: str) -> bool:
+    if not isinstance(value, str):
+        raise TypeError(
+            f"is_0x_prefixed requires text typed arguments. Got: {repr(value)}"
+        )
+    return value.startswith(("0x", "0X"))
+
+
+def remove_0x_prefix(value: HexStr) -> HexStr:
+    if is_0x_prefixed(value):
+        return HexStr(value[2:])
+    return value
+
+
+def add_0x_prefix(value: HexStr) -> HexStr:
+    if is_0x_prefixed(value):
+        return value
+    return HexStr("0x" + value)
